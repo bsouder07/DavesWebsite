0c9d779c6a4157a330274737a49e7c4c5a893088
Brian Souder <bsoudee@yahoo.com>
2025-05-16 21:19:19 -0400

    Prepare final deploy with custom domain

--- daves-website/vite.config.js
+++ daves-website/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // because you're using a custom domain
+  base: '/', // or '/' if using a custom domain
   build: {
-    outDir: 'docs',
-  },
-})
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
+});
